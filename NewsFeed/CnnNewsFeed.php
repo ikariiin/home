7d57e0cfa202723aae0588080c96d7090cb34f58
gourabNagDev <gourabnag12@gmail.com>
2015-06-12 01:22:46 +0530

    updated CNNParser

--- NewsFeed/CnnNewsFeed.php
+++ NewsFeed/CnnNewsFeed.php
@@ -7,5 +7,29 @@
  */
 
 class CnnNewsFeed {
+    private $url;
+    private $error;
+    private $simpleXmlObj;
+    public function __construct($url)
+    {
+        $this->url = $url;
+    }
+    private function get_content()
+    {
+        $header = @get_headers($this->url);
+        if($header[0] = "HTTP/1.1 404 Not Found")
+        {
+            $this->error = array("exist" => false);
+        }
+        else
+        {
+            $this->simpleXmlObj = simplexml_load_string(file_get_contents($this->url));
+            return $this->simpleXmlObj;
+        }
+    }
+    private function parse()
+    {
+        $handle = $this->get_content();
 
+    }
 }
